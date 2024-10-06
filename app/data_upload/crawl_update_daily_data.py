d29a0dbda49a8a13da2460a0c49ca5a21b92d384
장해원/에디(Eddie) <145424895+penum1227@users.noreply.github.com>
2024-10-06 23:18:48 +0900

    gpu 제거 추가

--- app/data_upload/crawl_update_daily_data.py
+++ app/data_upload/crawl_update_daily_data.py
@@ -39,6 +39,8 @@ def get_chrome_driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")  # 추가
+    chrome_options.add_argument("--remote-debugging-port=9222")  # 추가
 
     # Chrome WebDriver 실행
     return webdriver.Chrome(options=chrome_options)
