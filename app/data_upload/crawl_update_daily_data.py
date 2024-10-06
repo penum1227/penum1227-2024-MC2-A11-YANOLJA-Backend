d6666ac2a414a763f96fb08955ec9badf5a22963
장해원/에디(Eddie) <145424895+penum1227@users.noreply.github.com>
2024-10-07 00:45:07 +0900

    크롬 버전과 드라이버 버전 매치 작업 수정

--- app/data_upload/crawl_update_daily_data.py
+++ app/data_upload/crawl_update_daily_data.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
